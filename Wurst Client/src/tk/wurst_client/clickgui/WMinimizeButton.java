9b00702d9916fae0977ccfc5799e68a84563de62
Alexander01998 <contact.wurstimperium@gmail.com>
2015-04-20 16:19:45 +0200

    :art: Add hover & pressed icon for minimize button

--- Wurst Client/src/tk/wurst_client/clickgui/WMinimizeButton.java
+++ Wurst Client/src/tk/wurst_client/clickgui/WMinimizeButton.java
@@ -14,31 +14,36 @@ import java.awt.event.ActionEvent;
 public class WMinimizeButton extends WButton
 {
 	@Override
-	protected void drawIcon(Graphics2D g)
-	{
-		g.setColor(new Color(64, 64, 64, 128));
-		g.fillRect(0, 0, 28, 28);
-		g.setColor(Color.BLACK);
-		g.drawPolygon(new int[]{2, 26, 14}, new int[]{8, 8, 22}, 3);
-		g.setColor(new Color(0, 255, 0, 64));
-		g.fillPolygon(new int[]{2, 26, 14}, new int[]{8, 8, 22}, 3);
-	}
-	
-	@Override
 	protected void click(ActionEvent e)
 	{
 		
 	}	
+	
+	@Override
+	protected void drawIcon(Graphics2D g)
+	{
+		drawGreenIcon(g, 64);
+	}
 
 	@Override
 	protected void drawHoverIcon(Graphics2D g)
 	{
-		
+		drawGreenIcon(g, 128);
 	}
 
 	@Override
 	protected void drawPressedIcon(Graphics2D g)
 	{
-		
+		drawGreenIcon(g, 192);
+	}
+	
+	private void drawGreenIcon(Graphics2D g, int opacity)
+	{
+		g.setColor(new Color(64, 64, 64, 128));
+		g.fillRect(0, 0, 28, 28);
+		g.setColor(new Color(0, 0, 0, 128));
+		g.drawPolygon(new int[]{2, 26, 14}, new int[]{8, 8, 22}, 3);
+		g.setColor(new Color(0, 255, 0, opacity));
+		g.fillPolygon(new int[]{2, 26, 14}, new int[]{8, 8, 22}, 3);
 	}
 }
